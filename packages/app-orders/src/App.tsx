88bde8c2b3f67534f354b8cef80b89eab5b79a47
Giuseppe Ciotola <30926550+gciotola@users.noreply.github.com>
2023-01-25 11:49:13 +0100

    feat: add basic vite + preact app configuration

---
+++ packages/app-orders/src/App.tsx
@@ -0,0 +1,6 @@
+import { PageLayout } from '@commercelayer/core-app-elements'
+import { JSX } from 'preact'
+
+export function App(): JSX.Element {
+  return <PageLayout title='Orders'>Hello from orders app</PageLayout>
+}
