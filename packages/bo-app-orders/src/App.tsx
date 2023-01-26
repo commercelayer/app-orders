32da2b95754afced4f8d54a29ff8aef7f02b6b1d
Giuseppe Ciotola <30926550+gciotola@users.noreply.github.com>
2023-01-26 15:49:30 +0100

    chore: rename package folder as bo-app-orders

--- packages/bo-app-orders/src/App.tsx
+++ packages/bo-app-orders/src/App.tsx
@@ -1,5 +1,5 @@
 import { PageLayout } from '@commercelayer/core-app-elements'
-import { JSX } from 'preact'
+import type { JSX } from 'preact'
 
 export function App(): JSX.Element {
   return <PageLayout title='Orders'>Hello from orders app</PageLayout>
