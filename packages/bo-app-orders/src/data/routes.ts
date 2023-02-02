4ae8de82ab07952c87541f120f662b745539b694
Giuseppe Ciotola <30926550+gciotola@users.noreply.github.com>
2023-02-02 15:30:03 +0100

    test: add test case for appRoutes

--- packages/bo-app-orders/src/data/routes.ts
+++ packages/bo-app-orders/src/data/routes.ts
@@ -1,3 +1,5 @@
+export type AppRoute = keyof typeof appRoutes
+
 // Object to be used as source of truth to handel application routes
 // each page should correspond to a key and each key should have
 // a `path` property to be used as patter matching in <Route path> component
