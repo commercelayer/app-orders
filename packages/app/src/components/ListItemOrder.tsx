093a6743cc25fe0e0f02eaa16afd3ec601b7f5b8
Giuseppe Ciotola <30926550+gciotola@users.noreply.github.com>
2023-03-22 12:16:00 +0100

    fix: add anchor tag to `ListItemOrder`

--- packages/app/src/components/ListItemOrder.tsx
+++ packages/app/src/components/ListItemOrder.tsx
@@ -10,7 +10,7 @@ import {
   withinSkeleton
 } from '@commercelayer/app-elements'
 import type { Order } from '@commercelayer/sdk'
-import { useLocation } from 'wouter'
+import { Link } from 'wouter'
 import { makeOrder } from '#mocks'
 
 interface Props {
@@ -23,43 +23,46 @@ function ListItemOrderComponent({
   const {
     settings: { timezone }
   } = useTokenProvider()
-  const [, setLocation] = useLocation()
 
   const displayStatus = getDisplayStatus(resource)
   return (
-    <ListItem
-      icon={
-        <Icon
-          name={displayStatus.icon}
-          background={displayStatus.color}
-          gap='large'
-        />
-      }
-      onClick={() => setLocation(appRoutes.details.makePath(resource.id))}
-    >
-      <div>
-        <Text tag='div' weight='semibold'>
-          {resource.market?.name} #{resource.number}
-        </Text>
-        <Text tag='div' weight='medium' size='small' variant='info'>
-          {displayStatus.label} · {resource.customer?.email} ·{' '}
-          {formatDate({ isoDate: resource.updated_at, timezone })}
-        </Text>
-        {displayStatus.task != null && (
-          <Text tag='div' weight='bold' size='small' variant='warning'>
-            {displayStatus.task}
-          </Text>
-        )}
-      </div>
-      <div>
-        <Text tag='div' weight='semibold'>
-          {resource.formatted_total_amount}
-        </Text>
-        <Text tag='div' weight='medium' size='small' variant='info'>
-          {getPaymentStatusName(resource.payment_status)}
-        </Text>
-      </div>
-    </ListItem>
+    <Link href={appRoutes.details.makePath(resource.id)}>
+      <a>
+        <ListItem
+          icon={
+            <Icon
+              name={displayStatus.icon}
+              background={displayStatus.color}
+              gap='large'
+            />
+          }
+          onClick={() => {}}
+        >
+          <div>
+            <Text tag='div' weight='semibold'>
+              {resource.market?.name} #{resource.number}
+            </Text>
+            <Text tag='div' weight='medium' size='small' variant='info'>
+              {displayStatus.label} · {resource.customer?.email} ·{' '}
+              {formatDate({ isoDate: resource.updated_at, timezone })}
+            </Text>
+            {displayStatus.task != null && (
+              <Text tag='div' weight='bold' size='small' variant='warning'>
+                {displayStatus.task}
+              </Text>
+            )}
+          </div>
+          <div>
+            <Text tag='div' weight='semibold'>
+              {resource.formatted_total_amount}
+            </Text>
+            <Text tag='div' weight='medium' size='small' variant='info'>
+              {getPaymentStatusName(resource.payment_status)}
+            </Text>
+          </div>
+        </ListItem>
+      </a>
+    </Link>
   )
 }
 
