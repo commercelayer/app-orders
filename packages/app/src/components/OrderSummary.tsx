d7aec2ef0a04533480ac4c2bf2e63da19efa82d3
Marco Montalbano <marco.montalbano@commercelayer.io>
2023-08-21 20:52:01 +0200

    chore: increase OrderSummary readability

--- packages/app/src/components/OrderSummary.tsx
+++ packages/app/src/components/OrderSummary.tsx
@@ -28,7 +28,6 @@ type FooterActions = NonNullable<
 
 export const OrderSummary = withSkeletonTemplate<Props>(
   ({ order }): JSX.Element => {
-    const { sdkClient } = useCoreSdkProvider()
     const { mutateOrder } = useOrderDetails(order.id)
     const { triggerAttributes } = getOrderDisplayStatus(order)
 
@@ -38,74 +37,10 @@ export const OrderSummary = withSkeletonTemplate<Props>(
       useCaptureOverlay()
     const { show: showCancelOverlay, Overlay: CancelOverlay } =
       useCancelOverlay()
-    const { show: showAddItemOverlay, Overlay: AddItemOverlay } =
-      useAddItemOverlay()
     const {
       show: showSelectShippingMethodOverlay,
       Overlay: SelectShippingMethodOverlay
     } = useSelectShippingMethodOverlay()
-
-    const hasInvalidShipments = useMemo(
-      () =>
-        (order.shipments?.filter((shipment) => shipment.shipping_method == null)
-          ?.length ?? 0) > 0,
-      [order]
-    )
-
-    const hasInvalidOrderAmount = useMemo(
-      () => false,
-      // () => (order.total_amount_cents ?? Infinity) > 9900,
-      [order]
-    )
-
-    const canEdit =
-      order.status === 'placed' && order.payment_status !== 'unpaid'
-
-    const editingFooterActions: FooterActions = useMemo(() => {
-      if (order.status !== 'editing') {
-        return []
-      }
-
-      const cancelAction: FooterActions[number] = {
-        label: getOrderTriggerAttributeName('_cancel'),
-        variant: 'secondary',
-        disabled: isLoading,
-        onClick: () => {
-          showCancelOverlay()
-        }
-      }
-
-      const continueAction: FooterActions[number] = {
-        label: 'Continue',
-        disabled: isLoading,
-        onClick: () => {
-          showSelectShippingMethodOverlay()
-        }
-      }
-
-      const finishAction: FooterActions[number] = {
-        label: 'Finish',
-        disabled: isLoading,
-        onClick: () => {
-          void dispatch('_stop_editing')
-        }
-      }
-
-      return [
-        cancelAction,
-        hasInvalidShipments || hasInvalidOrderAmount
-          ? continueAction
-          : finishAction
-      ]
-    }, [
-      getOrderTriggerAttributeName,
-      isLoading,
-      order.status,
-      showCancelOverlay,
-      hasInvalidShipments,
-      hasInvalidOrderAmount,
-      dispatch
-    ])
 
     const standardFooterActions: FooterActions = useMemo(() => {
       return triggerAttributes
@@ -148,40 +83,59 @@ export const OrderSummary = withSkeletonTemplate<Props>(
       triggerAttributes
     ])
 
-    return (
-      <Section
-        title='Summary'
-        actionButton={
-          <>
-            {canEdit && (
-              <>
-                <Button
-                  variant='link'
-                  onClick={(e) => {
-                    e.preventDefault()
-                    void dispatch('_start_editing')
-                  }}
-                >
-                  Edit
-                </Button>
-              </>
-            )}
+    const editingFooterActions: FooterActions = useMemo(() => {
+      if (order.status !== 'editing') {
+        return []
+      }
 
-            {order.status === 'editing' && (
-              <>
-                <Button
-                  variant='link'
-                  onClick={() => {
-                    showAddItemOverlay()
-                  }}
-                >
-                  Add item
-                </Button>
-              </>
-            )}
-          </>
+      const cancelAction: FooterActions[number] = {
+        label: getOrderTriggerAttributeName('_cancel'),
+        variant: 'secondary',
+        disabled: isLoading,
+        onClick: () => {
+          showCancelOverlay()
         }
-      >
+      }
+
+      const continueAction: FooterActions[number] = {
+        label: 'Continue',
+        disabled: isLoading,
+        onClick: () => {
+          showSelectShippingMethodOverlay()
+        }
+      }
+
+      const finishAction: FooterActions[number] = {
+        label: 'Finish',
+        disabled: isLoading,
+        onClick: () => {
+          void dispatch('_stop_editing')
+        }
+      }
+
+      const hasInvalidShipments =
+        (order.shipments?.filter((shipment) => shipment.shipping_method == null)
+          ?.length ?? 0) > 0
+
+      const hasInvalidOrderAmount = false
+      // const hasInvalidOrderAmount = (order.total_amount_cents ?? Infinity) > 9900
+
+      return [
+        cancelAction,
+        hasInvalidShipments || hasInvalidOrderAmount
+          ? continueAction
+          : finishAction
+      ]
+    }, [
+      getOrderTriggerAttributeName,
+      isLoading,
+      order,
+      showCancelOverlay,
+      dispatch
+    ])
+
+    return (
+      <Section title='Summary' actionButton={<ActionButton order={order} />}>
         <ResourceOrderSummary
           order={order}
           editable={order.status === 'editing'}
@@ -207,28 +161,7 @@ export const OrderSummary = withSkeletonTemplate<Props>(
           }}
         />
 
-        <AddItemOverlay
-          onConfirm={({ type, code }) => {
-            void sdkClient.line_items
-              .create({
-                order: sdkClient.orders.relationship(order.id),
-                quantity: 1,
-                ...(type === 'skus'
-                  ? { sku_code: code }
-                  : { bundle_code: code })
-              })
-              .then(async () => {
-                return await mutateOrder()
-              })
-          }}
-        />
-
-        <SelectShippingMethodOverlay
-          order={order}
-          onConfirm={() => {
-            alert('asd')
-          }}
-        />
+        <SelectShippingMethodOverlay order={order} />
       </Section>
     )
   }
@@ -246,4 +179,61 @@ function renderErrorMessages(errors?: string[]): JSX.Element {
   ) : (
     <></>
   )
+}
+
+const ActionButton: React.FC<{ order: Order }> = ({ order }) => {
+  const { sdkClient } = useCoreSdkProvider()
+  const { dispatch } = useTriggerAttribute(order.id)
+  const { mutateOrder } = useOrderDetails(order.id)
+  const { show: showAddItemOverlay, Overlay: AddItemOverlay } =
+    useAddItemOverlay()
+
+  const canEdit = order.status === 'placed' && order.payment_status !== 'unpaid'
+  const isEditing = order.status === 'editing'
+
+  if (canEdit) {
+    return (
+      <Button
+        variant='link'
+        onClick={(e) => {
+          e.preventDefault()
+          void dispatch('_start_editing')
+        }}
+      >
+        Edit
+      </Button>
+    )
+  }
+
+  if (isEditing) {
+    return (
+      <>
+        <AddItemOverlay
+          onConfirm={({ type, code }) => {
+            void sdkClient.line_items
+              .create({
+                order: sdkClient.orders.relationship(order.id),
+                quantity: 1,
+                ...(type === 'skus'
+                  ? { sku_code: code }
+                  : { bundle_code: code })
+              })
+              .then(async () => {
+                return await mutateOrder()
+              })
+          }}
+        />
+        <Button
+          variant='link'
+          onClick={() => {
+            showAddItemOverlay()
+          }}
+        >
+          Add item
+        </Button>
+      </>
+    )
+  }
+
+  return null
 }
