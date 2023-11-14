bc021c569957d410f555edc548a0a77bd8dde505
Marco Montalbano <marco.montalbano@commercelayer.io>
2023-11-14 20:43:39 +0100

    chore: remove elements's order-summary

--- packages/app/src/pages/OrderDetails.tsx
+++ packages/app/src/pages/OrderDetails.tsx
@@ -6,7 +6,6 @@ import { OrderReturns } from '#components/OrderReturns'
 import { OrderShipments } from '#components/OrderShipments'
 import { OrderSteps } from '#components/OrderSteps'
 import { OrderSummary } from '#components/OrderSummary'
-import { OrderSummaryFromElements } from '#components/OrderSummaryFromElements'
 import { ScrollToTop } from '#components/ScrollToTop'
 import { Timeline } from '#components/Timeline'
 import { appRoutes } from '#data/routes'
@@ -122,9 +121,6 @@ export function OrderDetails(): JSX.Element {
           </Spacer>
           <Spacer top='14'>
             <OrderSummary order={order} />
-          </Spacer>
-          <Spacer top='14'>
-            <OrderSummaryFromElements order={order} />
           </Spacer>
           <Spacer top='14'>
             <OrderCustomer order={order} />
